4d62d152ffef4e07b781f1087729067377093a15
Saif Kbishi <Saifkbishi@gmail.com>
2021-04-17 14:59:12 +0300

    mybank first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const chalk = require('chalk');
+const app = express();
+const PORT = 3002;
+app.use(express.json());//now express can understand JSON 
+const {displayAllAccounts, addNewAccount, changeAccountStatus, displayOneAccount, updateAccountDetails, deleteOneAcount, AddDeposit, withdrawMoney, updateAccountCredit, transferMoney, } = require("./utils");
+const entity = 'account';
+
+//toggle account status by name
+app.put(`/api/accounts/togglestatus/:name`, (req, res)=>{
+ const {name} = req.params;
+ const {IsActive} = req.body;
+ console.log(req.params, req.body);
+ console.log('put IsActive', IsActive);
+ const toggleAccountStatus = changeAccountStatus(name, IsActive);
+ res.status(200).send(toggleAccountStatus);
+});
+
+//delete an account
+app.delete(`/api/accounts/delete/:name`, (req, res)=>{
+ const {name} = req.params;
+ const deleteAccount = deleteOneAcount(name);
+ res.status(200).send(deleteAccount);
+});
+
+//create one record
+app.post(`/api/accounts/create`, (req, res)=>{
+ //console.log('req.body',req.body,'this is from Post to create new record');
+ try{
+  const createRecord = addNewAccount(req.body);
+  res.status(201).send(createRecord); //status 201 means successfully created an object by default 
+ }catch(error){
+  console.log('could not create new record');
+  res.status(400).send({error: message});//whenever the user gives you bad data at the client you 
+                                         //want to send a 400 error because that means there's something wrong with the
+                                         //user input and not something wrong with your server
+ }
+});
+
+//retrieve All Accounts
+app.get(`/api/${entity}s/allaccounts`, (req, res)=>{
+ const allAccount = displayAllAccounts();
+ res.status(200).send(allAccount);
+});
+
+app.listen(PORT, ()=>console.log(`server listening to port `+ chalk.blue.underline.bold(`${PORT}`)));
+
+
+/**
+ * localhost:3002/api/accounts
+ * id
+ * ppID
+ * name
+ * cash
+ * credit
+ * IsActive
+ */
---
+++ utils.js
@@ -0,0 +1,111 @@
+const chalk = require('chalk');
+const fs = require('fs');
+const DB = './db/bank.json'
+const entity = 'account';
+
+//try{}catch(error){console.log(`Could not toggle Account status: ${error}`)}
+//if(){}else{}
+
+const AddDeposit =()=>{
+ console.log('trying to add Deposit');
+}//AddDeposit
+
+const changeAccountStatus = (name, IsActive)=>{
+ console.log('trying to TOGGLE account status');
+ // console.log('10', name, IsActive)
+ const accounts = displayAllAccounts();
+ let accountFound=findAccountByNameOrppID(accounts, name, IsActive);
+ try{  
+  if(accountFound){
+   console.log('14 current stauts',accountFound.IsActive );
+   accountFound.IsActive = !accountFound.IsActive;
+   console.log(`Account name ${name} status was modified: `,accountFound.IsActive );
+   saveAccounts(accounts);
+  }else{
+   console.log(`Account with name=${name}, was not found.`);
+  }
+ }catch(error){console.log(`Could not toggle Account status: ${error}`)}
+}//changeAccountStatus
+
+const findAccountByNameOrppID = (accounts, name='', ppID=0)=>{
+ console.log(name, ppID);
+ let accountFound;
+ if(accountFound = accounts.find((account)=>{return account.name == name;}) )
+ {
+  //console.log('accountFound',accountFound);
+  return accountFound;//returns undefined if account is not found else  returns the accoutn
+ }
+ else if(accountFound = accounts.find((account)=>{return account.ppID == ppID;})) //this is a random number
+ {
+  //console.log('accountFound',accountFound);
+  return accountFound;//returns undefined if account is not found else  returns the accoutn
+ }
+}//findAccountByNameOrppID
+
+const addNewAccount = (argv)=>{//newly created accounts are Active be defaut
+ console.log('trying to add new account');
+ try{
+  const accounts = displayAllAccounts();
+  const accountExists = findAccountByNameOrppID(accounts, argv.name, argv.ppID); 
+  if(!accountExists){
+   console.log(chalk.green(`Account with Name=${argv.name} will be created`));
+   accounts.push({
+    id: argv.id,
+    ppID: argv.ppID,
+    name: argv.name, 
+    cash: argv.cash,
+    credit: argv.credit, 
+    IsActive: argv.IsActive
+   });
+   saveAccounts(accounts);
+  }else{
+   console.log(`Account with PassPortID=`+ chalk.red(argv.ppID) +` or Name=`+ chalk.red(argv.name) +` already exits. \nAnd will not be created`);
+  }
+ }catch(error){console.log(`Could not create new Account: ${error}`)}
+}//addNewAccount
+
+const deleteOneAcount = (name) =>{
+ console.log('trying to DELETE one Account');
+ const accounts = displayAllAccounts();
+ const findAccount = accounts.find((account)=>{
+  return account.name = name;
+ });
+ if(findAccount){
+  const filteredAccount = accounts.filter((account)=>{
+   return account.name !== name;
+  });
+  saveAccounts(filteredAccount);
+ }else{
+  console.log(`Account with name=${name}, could not be found. \nAnd cannot be deleted!`)
+ }
+}//deleteOneAcount
+
+const saveAccounts =(accounts)=>{
+ const dataJSON = JSON.stringify(accounts);
+ fs.writeFileSync(`${DB}`, dataJSON);
+}//saveAccounts
+
+const displayAllAccounts = ()=>{
+ console.log(chalk.green('Retriveing all accounts in the Bank'));
+ try{
+  const dataBuffer = fs.readFileSync(`${DB}`);
+  const dataJSON = dataBuffer.toString();
+  return JSON.parse(dataJSON);
+ }catch(error){
+  console.log('No Accounts to return')
+  return [];
+ }
+}//displayAllAccounts
+
+module.exports ={
+ displayAllAccounts, 
+ addNewAccount, 
+ changeAccountStatus, 
+ // displayOneAccount, 
+ // updateAccountDetails, 
+ deleteOneAcount, 
+ AddDeposit, 
+ // withdrawMoney, 
+ // updateAccountCredit, 
+ // transferMoney,
+};
